ef5e6ab2906dcbdad034cdc52788e9b1a9bee99e
Alfonso S. Michaelis <ntweir@andrew.cmu.edu>
2017-12-05 14:33:43 -0500

    reset

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,17 +5,3 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
-subdirs(slam/hector_slam)
-subdirs(slam/hector_slam_launch)
-subdirs(slam/hector_map_tools)
-subdirs(slam/hector_nav_msgs)
-subdirs(slam/hector_geotiff)
-subdirs(slam/hector_geotiff_plugins)
-subdirs(slam/hector_marker_drawing)
-subdirs(slam/hector_compressed_map_transport)
-subdirs(rplidar)
-subdirs(slam/hector_imu_attitude_to_tf)
-subdirs(slam/hector_imu_tools)
-subdirs(slam/hector_map_server)
-subdirs(slam/hector_trajectory_server)
-subdirs(slam/hector_mapping)
